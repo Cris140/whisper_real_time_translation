28b113a185c31b4d008ffeaba55ed03df66ddbe3
Cristhian <60457418+Cris140@users.noreply.github.com>
2024-08-08 15:12:46 -0400

    Update translation_demo.py

--- translation_demo.py
+++ translation_demo.py
@@ -2,14 +2,9 @@ import argparse
 import io
 import os
 import speech_recognition as sr
-import whisper
 import torch
-import subprocess
 import nltk
 from nltk.tokenize import sent_tokenize
-
-
-
 
 from datetime import datetime, timedelta
 from queue import Queue
@@ -22,28 +17,27 @@ from TranscriptionWindow import TranscriptionWindow
     
 def main():
     
-   
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="medium", help="Model to use",
                         choices=["tiny", "base", "small", "medium", "large"])
     parser.add_argument("--device", default="auto", help="device to user for CTranslate2 inference",
-                        choices=["auto", "cuda","cpu"])                   
+                        choices=["auto", "cuda", "cpu"])
     parser.add_argument("--compute_type", default="auto", help="Type of quantization to use",
-                        choices=["auto", "int8", "int8_floatt16", "float16", "int16", "float32"])
+                        choices=["auto", "int8", "int8_float16", "float16", "int16", "float32"])
     parser.add_argument("--translation_lang", default='English',
-                        help="Which language should we translate into." , type=str)
+                        help="Which language should we translate into.", type=str)
+    parser.add_argument("--input_lang", default='en', help="Language of the input audio.", type=str)
     parser.add_argument("--non_english", action='store_true',
-                        help="Don't use the english model.")
+                        help="Don't use the English model.")
     parser.add_argument("--threads", default=0,
-                        help="number of threads used for CPU inference", type=int)
+                        help="Number of threads used for CPU inference", type=int)
     parser.add_argument("--energy_threshold", default=1000,
                         help="Energy level for mic to detect.", type=int)
     parser.add_argument("--record_timeout", default=2,
-                        help="How real time the recording is in seconds.", type=float)
-                        
+                        help="How real-time the recording is in seconds.", type=float)
     parser.add_argument("--phrase_timeout", default=3,
                         help="How much empty space between recordings before we "
-                             "consider it a new line in the transcription.", type=float) 
+                             "consider it a new line in the transcription.", type=float)
                              
     if 'linux' in platform:
         parser.add_argument("--default_microphone", default='pulse',
@@ -51,19 +45,19 @@ def main():
                                  "Run this with 'list' to view available Microphones.", type=str)
     args = parser.parse_args()
     
-    # The last time a recording was retreived from the queue.
+    # The last time a recording was retrieved from the queue.
     phrase_time = None
     # Current raw audio bytes.
     last_sample = bytes()
-    # Thread safe Queue for passing data from the threaded recording callback.
+    # Thread-safe Queue for passing data from the threaded recording callback.
     data_queue = Queue()
-    # We use SpeechRecognizer to record our audio because it has a nice feauture where it can detect when speech ends.
+    # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
     recorder = sr.Recognizer()
     recorder.energy_threshold = args.energy_threshold
-    # Definitely do this, dynamic energy compensation lowers the energy threshold dramtically to a point where the SpeechRecognizer never stops recording.
+    # Definitely do this; dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
     recorder.dynamic_energy_threshold = False
     
-    # Important for linux users. 
+    # Important for Linux users. 
     # Prevents permanent application hang and crash by using the wrong Microphone
     if 'linux' in platform:
         mic_name = args.default_microphone
@@ -96,8 +90,9 @@ def main():
     cpu_threads = args.threads
     
     nltk.download('punkt')
-    audio_model = WhisperModel(model, device = device, compute_type = compute_type , cpu_threads = cpu_threads)
+    audio_model = WhisperModel(model, device=device, compute_type=compute_type, cpu_threads=cpu_threads)
     window = TranscriptionWindow()
+    translator = GoogleTranslate()
     
     record_timeout = args.record_timeout
     phrase_timeout = args.phrase_timeout
@@ -108,17 +103,17 @@ def main():
     with source:
         recorder.adjust_for_ambient_noise(source)
 
-    def record_callback(_, audio:sr.AudioData) -> None:
+    def record_callback(_, audio: sr.AudioData) -> None:
         """
-        Threaded callback function to recieve audio data when recordings finish.
+        Threaded callback function to receive audio data when recordings finish.
         audio: An AudioData containing the recorded bytes.
         """
-        # Grab the raw bytes and push it into the thread safe queue.
+        # Grab the raw bytes and push it into the thread-safe queue.
         data = audio.get_raw_data()
         data_queue.put(data)
 
     # Create a background thread that will pass us raw audio bytes.
-    # We could do this manually but SpeechRecognizer provides a nice helper.
+    # We could do this manually, but SpeechRecognizer provides a nice helper.
     recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
 
     # Cue the user that we're ready to go.
@@ -154,13 +149,12 @@ def main():
                 # Read the transcription.
                 text = ""
                     
-                segments, info = audio_model.transcribe(temp_file)
+                segments, info = audio_model.transcribe(temp_file, language=args.input_lang)
                 for segment in segments:
                     text += segment.text
-                #text = result['text'].strip()
 
-                # If we detected a pause between recordings, add a new item to our transcripion.
-                # Otherwise edit the existing one.
+                # If we detected a pause between recordings, add a new item to our transcription.
+                # Otherwise, edit the existing one.
                 if phrase_complete:
                     transcription.append(text)
                 else:
@@ -168,9 +162,11 @@ def main():
                 last_four_elements = transcription[-10:]
                 result = ''.join(last_four_elements)    
                 sentences = sent_tokenize(result)
-                window.update_text(sentences, translation_lang)
-                # Clear the console to reprint the updated transcription.
-
+                
+                # Translate the text
+                translated_sentences = [translator.translate(sentence, translation_lang).result for sentence in sentences]
+                window.update_text(translated_sentences, translation_lang)
+                
                 # Infinite loops are bad for processors, must sleep.
                 sleep(0.25)
         except KeyboardInterrupt:
@@ -182,5 +178,4 @@ def main():
 
 
 if __name__ == "__main__":
-    
     main()
